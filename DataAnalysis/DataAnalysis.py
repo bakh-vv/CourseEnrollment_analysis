fb5ce9cb93e32d3bd2ca0859392c6619c4a6a450
softwaretm <beisenov_b_b@mail.ru>
2018-06-17 21:55:04 +0300

    Import CSV data

--- DataAnalysis/DataAnalysis.py
+++ DataAnalysis/DataAnalysis.py
@@ -1,1 +1,15 @@
+import unicodecsv
+
+enrollments = []
+f = open('enrollments.csv', 'rb')
+reader = unicodecsv.DictReader(f) #creates an iterable, which can only be used once
+
+for row in reader:
+    enrollments.append(row) #to store it permanently we put it in a list
+
+f.close()
+
+enrollments[0]
+
+
 
