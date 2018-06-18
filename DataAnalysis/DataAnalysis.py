1d507f657e2bbd57a48db78d0cdaf976b26a7e95
softwaretm <beisenov_b_b@mail.ru>
2018-06-18 22:41:44 +0300

    Renamed a column in a file

--- DataAnalysis/DataAnalysis.py
+++ DataAnalysis/DataAnalysis.py
@@ -51,9 +51,77 @@ enrollments = read_csv('enrollments.csv')
 daily_engagement = read_csv('daily_engagement.csv')
 project_submissions = read_csv('project_submissions.csv')
 
+#print(enrollments[0])
+
+#print(daily_engagement[0])
+#print(project_submissions[0])
+
+
+### Fixing Data Types
+
+from datetime import datetime as dt
+
+def parse_date(date):
+    if date == '':
+        return None
+    else:
+        return dt.strptime(date, '%Y-%m-%d')
+
+def parse_maybe_int(i):
+    if i == '':
+        return None
+    else:
+        return int(i)
+
+# Clean up the data types in the enrollments table
+for enrollment in enrollments:
+    enrollment['cancel_date'] = parse_date(enrollment['cancel_date'])
+    enrollment['days_to_cancel'] = parse_maybe_int(enrollment['days_to_cancel'])
+    enrollment['is_canceled'] = enrollment['is_canceled'] == 'True'
+    enrollment['is_udacity'] = enrollment['is_udacity'] == 'True'
+    enrollment['join_date'] = parse_date(enrollment['join_date'])
+
 print(enrollments[0])
 
+# Clean up the data types in the engagement table
+for engagement_record in daily_engagement:
+    engagement_record['lessons_completed'] = int(float(engagement_record['lessons_completed']))
+    engagement_record['num_courses_visited'] = int(float(engagement_record['num_courses_visited']))
+    engagement_record['projects_completed'] = int(float(engagement_record['projects_completed']))
+    engagement_record['total_minutes_visited'] = float(engagement_record['total_minutes_visited'])
+    engagement_record['utc_date'] = parse_date(engagement_record['utc_date'])
+    
 print(daily_engagement[0])
+
+
+# Clean up the data types in the submissions table
+for submission in project_submissions:
+    submission['completion_date'] = parse_date(submission['completion_date'])
+    submission['creation_date'] = parse_date(submission['creation_date'])
+
 print(project_submissions[0])
 
 
+#enrollment_num_rows = len(enrollments)
+#enrollment_num_unique_students = len(set([dic['account_key'] for dic in enrollments]))
+
+#engagement_num_rows = len(daily_engagement)
+#engagement_num_unique_students = len(set([dic['acct'] for dic in daily_engagement]))
+
+#submission_num_rows = len(project_submissions)          
+#submission_num_unique_students = len(set([dic['account_key'] for dic in project_submissions]))
+
+print(enrollment_num_rows)
+print(engagement_num_rows)
+print(submission_num_rows)
+
+print(enrollment_num_unique_students)
+print(engagement_num_unique_students)
+print(submission_num_unique_students)
+
+#renaming column name, to keep it the same among the 3 files, by creating a new one and then deleting the old one
+for dict in daily_engagement:
+    dict['account_key'] = dict['acct']
+
+for dict in daily_engagement:
+    del dict['acct']
